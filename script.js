c0b177d8184aadfc738e9d17a7fa1d6d58941d94
Alexei <example@gmail.com>
2024-12-05 15:49:25 +0200

    MovieApp

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 "use strict";
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
-let lastFilm = prompt("Один из последних просмотренных фильмов?");
-let evaluateOfFilm = prompt("На сколько оцените его?");
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-        lFilm: lastFilm,
-        logan: evaluateOfFilm
-    },
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
-console.log(personalMovieDB);
-console.log(numberOfFilms);
-console.log(lastFilm);
-console.log(evaluateOfFilm);
+const a = prompt("Один из последних просмотренных фильмов?"),
+ b = prompt("На сколько оцените его?"),
+ c = prompt("Один из последних просмотренных фильмов?"),
+ d = prompt("На сколько оцените его?");
+ personalMovieDB.movies[a] = b;
+ personalMovieDB.movies[c] = d;
+
+ console.log(personalMovieDB);
 
 
 
