84f5361bf82c3773eae97428cc61b91d0ac3ceee
Alexei <example@gmail.com>
2025-01-14 13:11:17 +0200

    Ending of first course

--- script.js
+++ script.js
@@ -2,6 +2,96 @@
 
 
 
+////////////////////////////////////////////Async, defer////////////////////////
+// const p = document.querySelectorAll('p');
+// console.log(p);
+
+
+////////////////////////////////////////////СОБЫТИЯ НА МОБИЛЬНЫХ УСТРОЙСТВАХ////////////
+//touchstart
+//touchmove
+//touchend
+//touchenter
+//touchleave
+//touchcancel
+
+// window.addEventListener('DOMContentLoaded', () =>{
+//     const box = document.querySelector('.box');
+
+//     box.addEventListener('touchstart', (e)=>{
+//         e.preventDefault();
+
+//         console.log('Start');
+//     });
+
+//     box.addEventListener('touchmove', (e)=>{
+//         e.preventDefault();
+
+//         console.log('Move');
+//     });
+
+//     box.addEventListener('touchend', (e)=>{
+//         e.preventDefault();
+
+//         console.log('End');
+//     });
+// });
+
+//touches
+//targetTouches
+//changedTouches
+
+
+/////////////////////////////////////////НАВИГАЦИЯ ПО DOM///////////////////////////////
+//console.log(document.head);
+//console.log(document.documentElement);
+//console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.firstElementChild);
+
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentElement);
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+// for(let node of document.body.childNodes){
+//     if(node.nodeName == '#text'){
+//         continue;
+//     }
+
+//     console.log(node);
+// };
+
+//////////////////////////////////////////////////СОБЫТИЯ И ИХ ОБРАБОТЧИКИ////////////////
+// const btn = document.querySelector('button'),
+//       overlay = document.querySelector('.overlay');
+
+// // btn.onclick = function(){
+// //     alert('Click');
+// // };
+
+// // btn.onclick = function(){
+// //     alert('Second Click');
+// // };
+
+// let i = 0;
+// const deleteElement = (e) => {
+//     console.log(e.currentTarget);
+//     console.log(e.type);
+//     // i++;
+//     // if(i == 1){
+//     //     btn.removeEventListener('click', deleteElement);
+//     // }
+// };
+
+// btn.addEventListener('click', deleteElement);
+// overlay.addEventListener('click', deleteElement);
+
+
+
+
+
 //////////////////////////////////////////МЕТОДЫ И СВОЙСТВА/////////////////////////////
 // const str = "tEsT";
 
@@ -120,69 +210,69 @@
 //         break;
 // }
 /////////////////////////////////////////////ПРАКТИКА////////////////////////////////////
-let numberOfFilms;
-function start(){
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// let numberOfFilms;
+// function start(){
+//     numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-    while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-}
+//     while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+//         numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//     }
+// }
 
-start();
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// start();
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 
-function rememberMyFilms(){
-    for(let i = 0; i < 2; i++){
-        const a = prompt("Один из последних просмотренных фильмов?"),
-              b = prompt("На сколько оцените его?");
-         if(a != null && b != null && a != "" && b != "" && a.length < 50){
-             personalMovieDB.movies[a] = b;
-             console.log("done");
-         } else {
-             console.log("error");
-             i--;
-         }
+// function rememberMyFilms(){
+//     for(let i = 0; i < 2; i++){
+//         const a = prompt("Один из последних просмотренных фильмов?"),
+//               b = prompt("На сколько оцените его?");
+//          if(a != null && b != null && a != "" && b != "" && a.length < 50){
+//              personalMovieDB.movies[a] = b;
+//              console.log("done");
+//          } else {
+//              console.log("error");
+//              i--;
+//          }
      
          
-     }
-}
-rememberMyFilms();
+//      }
+// }
+// rememberMyFilms();
 
 
-function detectPersonalLevel(){
-    if(personalMovieDB.count < 10){
-        console.log("Просмотренно довольно мало фильмов");
-    } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-        console.log("Вы классический зритель");
-    } else if(personalMovieDB.count >= 30){
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
-detectPersonalLevel();
+// function detectPersonalLevel(){
+//     if(personalMovieDB.count < 10){
+//         console.log("Просмотренно довольно мало фильмов");
+//     } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+//         console.log("Вы классический зритель");
+//     } else if(personalMovieDB.count >= 30){
+//         console.log("Вы киноман");
+//     } else {
+//         console.log("Произошла ошибка");
+//     }
+// }
+// detectPersonalLevel();
 
-function showMyDB(hidden){
-    if (!hidden){
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
+// function showMyDB(hidden){
+//     if (!hidden){
+//         console.log(personalMovieDB);
+//     }
+// }
+// showMyDB(personalMovieDB.privat);
 
-function writeYourGenres(){
-    for(let i = 1; i <= 3; i++){
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-writeYourGenres();
+// function writeYourGenres(){
+//     for(let i = 1; i <= 3; i++){
+//         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+//     }
+// }
+// writeYourGenres();
 
 
 
